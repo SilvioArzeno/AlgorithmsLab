6ac02657afa80dbfb5e548b216a6ead5a00ab77c
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-02-07 10:27:08 -0400

    Optimized the las solution

--- PairSum/PairSumAlgorithm/PairSumAlgorithm/Program.cs
+++ PairSum/PairSumAlgorithm/PairSumAlgorithm/Program.cs
@@ -7,17 +7,15 @@ namespace PairSumAlgorithm
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 2, 3, 5, 8, 11, 4, 3 };
+            int[] numbers = new int[] { 2, 3, 5, 8, 11, 4, 3 , 5 , 12 , 13 , 22 , 18 , 25 };
             int counter = 0;
             Console.WriteLine("Give me a number to find how many pairs add up that number in the array: ");
             int sum = Convert.ToInt32(Console.ReadLine());
             Dictionary<string, int> Dnumbers = new Dictionary<string, int>();
             for (int i = 0; i < numbers.Length; i++)
-                if (!Dnumbers.ContainsKey(numbers[i].ToString()))
                 {
-                    Dnumbers.Add(numbers[i].ToString(), numbers[i]);
+                    Dnumbers.Add(i.ToString(), numbers[i]);
                 }
-                else Dnumbers.Add(numbers[i].ToString()+i.ToString(), numbers[i]);
             for (int i = 0; i < Dnumbers.Count; i++)
             {
                 if (Dnumbers.ContainsValue(sum - numbers[i]))
