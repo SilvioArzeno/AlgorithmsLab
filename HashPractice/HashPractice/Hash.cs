00f0e5f12c3ce2bd038c75a69099b53ed8f6c864
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-03-15 01:14:28 -0400

    Finished hash table

--- HashPractice/HashPractice/Hash.cs
+++ HashPractice/HashPractice/Hash.cs
@@ -13,6 +13,12 @@ namespace HashPractice
             public V value { get; set; }
             public bool Removed { get; set; }
 
+            public KVP(string _key, V _value)
+            {
+                key = _key;
+                value = _value;
+            }
+
         }
 
         public int Size { get; set; }
@@ -39,12 +45,11 @@ namespace HashPractice
                     pos = 0;
                 }
             }
-            throw new KeyNotFoundException();
+            return null;
         }
-
         private long Hash(string key)
         {
-            return 31L * Convert.ToInt32(key) + 8092435135;
+            return 31L * key.GetHashCode() + 8092435135;
         }
 
         public V Find(string key)
@@ -69,8 +74,80 @@ namespace HashPractice
             p.Removed = true;
             Size--;
             return p.value;
+
         }
 
-        public
+        public V Replace(string key, V value)
+        {
+            KVP p = Get(key);
+
+            if (p == null || p.Removed)
+            {
+                throw new KeyNotFoundException();
+            }
+            V old = p.value;
+            p.value = value;
+
+            return old;
+        }
+         public void add(string key, V value)
+        {
+            KVP p = Get(key);
+            if(p != null && !p.Removed)
+            {
+                throw new Exception("Duplicate key");
+            }
+            double LOAD_FACTOR = Size / arr.Length;
+
+            if(LOAD_FACTOR > 0.7)
+            {
+                Resize(2 * arr.Length);
+            }
+
+            int pos = Math.Abs((int)Hash(key) % arr.Length);
+           while(true)
+            {
+                if (arr[pos] == null || arr[pos].Removed)
+                {
+                    break;
+                }
+                pos++;
+                if(pos >= arr.Length)
+                {
+                    pos = 0;
+                }
+            }
+            arr[pos] = new KVP(key, value);
+            Size++;
+
+        }
+
+        private void Resize(int newcapacity)
+        {
+            KVP[] temp = new KVP[newcapacity];
+            foreach(KVP p in arr)
+            {
+                if(p == null || p.Removed)
+                {
+                    continue;
+                }
+                int pos = Math.Abs((int)Hash(p.key) % newcapacity);
+                while (true)
+                {
+                    if (temp[pos] == null)
+                    {
+                        break;
+                    }
+                    pos++;
+                    if (pos >= newcapacity)
+                    {
+                        pos = 0;
+                    }
+                }
+                temp[pos] = p;
+            }
+
+            arr = temp;
+        }
     }
 }
--- HashPractice/HashPractice/Program.cs
+++ HashPractice/HashPractice/Program.cs
@@ -6,7 +6,28 @@ namespace HashPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int> test = new Dictionary<int>();
+            test.add("Ocho", 8);
+            test.add("Nueve", 9);
+            test.add("Diez", 10);
+            test.add("Uno", 1);
+            test.add("Oops", 69);
+            Console.WriteLine(test.Find("Uno"));
+            test.Remove("Oops");
+            test.Replace("Ocho", 19);
+            test.add("Silvio", 99);
+            test.add("Enrique", 113);
+            Console.WriteLine(test.Find("Ocho"));
+            for(int i = 0; i < 100000; i++)
+            {
+                test.add(i.ToString(), i);
+            }
+            for (int i = 0; i < 100000; i++)
+            {
+                if(i % 2 == 0 || i % 3 == 0)
+                test.Remove(i.ToString());
+            }
+            Console.ReadKey();
         }
     }
 }
